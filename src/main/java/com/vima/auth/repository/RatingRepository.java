15fe0362d6df9e58a0878c99a1272dc734e4540c
Andrija25 <93194317+Andrija25@users.noreply.github.com>
2023-06-03 02:09:16 +0200

    added calculating avg rating for host feature

--- src/main/java/com/vima/auth/repository/RatingRepository.java
+++ src/main/java/com/vima/auth/repository/RatingRepository.java
@@ -2,8 +2,14 @@ package com.vima.auth.repository;
 
 import com.vima.auth.model.Rating;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface RatingRepository extends JpaRepository<Rating, Long> {
+
+    @Query(value = "SELECT count(r.id) FROM rating r WHERE r.host_id = ?1", nativeQuery = true)
+    int findAllByHostId(Long hostId);
 }
--- src/main/java/com/vima/auth/service/RatingService.java
+++ src/main/java/com/vima/auth/service/RatingService.java
@@ -1,6 +1,7 @@
 package com.vima.auth.service;
 
 import com.vima.auth.model.Rating;
+import com.vima.auth.model.User;
 import com.vima.auth.repository.RatingRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,8 +14,15 @@ import java.util.Random;
 public class RatingService {
 
     private final RatingRepository ratingRepository;
+    private final UserService userService;
 
     public Rating create(int value, Long hostId, Long guestId){
+       var newRating = executeRating(value, hostId, guestId);
+       calculateHostRating(value, hostId);
+       return newRating;
+    }
+
+    private Rating executeRating(int value, Long hostId, Long guestId){
         var newRating = Rating.builder()
                 .id(Math.abs(new Random().nextLong()))
                 .value(value)
@@ -26,4 +34,26 @@ public class RatingService {
         ratingRepository.save(newRating);
         return newRating;
     }
+
+    private void calculateHostRating(int value, Long hostId){
+        var host = userService.findById(hostId);
+        if (host.getAvgRating() == 0) {
+            calculateWhenZero(host, value);
+            return;
+        }
+
+        calculateWhenNotZero(host,value);
+    }
+
+    private void calculateWhenZero(User host, int value){
+        host.setAvgRating(value);
+        userService.save(host);
+    }
+
+    private void calculateWhenNotZero(User host, int value){
+        var numOfRatings = ratingRepository.findAllByHostId(host.getId());
+        host.setAvgRating(((host.getAvgRating()*(numOfRatings-1)) + value)/(numOfRatings));
+        userService.save(host);
+    }
+
 }
