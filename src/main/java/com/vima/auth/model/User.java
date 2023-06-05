0cbc174437299d58eb48f1fb8a8611669feeb2a9
Mihajlo Torbica <torbica.ra56.2019@uns.ac.rs>
2023-06-06 00:02:18 +0200

    necessery packages added

--- src/main/java/com/vima/auth/model/User.java
+++ src/main/java/com/vima/auth/model/User.java
@@ -1,12 +1,11 @@
 package com.vima.auth.model;
 
 import com.vima.auth.model.enums.Role;
+import javax.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.*;
 
 
 @Data
